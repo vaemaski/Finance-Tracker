b7157a02f8acc4cd40589ec884efd7bb6f0a1ed4
vaishali <vaishali.ds0228@gmail.com>
2025-08-08 00:51:01 +0530

    automated email for budget alerts done

--- actions/send-email.js
+++ actions/send-email.js
@@ -1,20 +1,20 @@
 import { Resend } from "resend";
-
+import { render } from "@react-email/render";
 
 export async function sendEmail({to, subject, react}){
     const resend = new Resend(process.env.RESEND_API_KEY || "");
 
     try {
         const data = await resend.emails.send({
-            from : "Finance App <onboarding@resend.dev>",
+            from: "Finance App <onboarding@resend.dev>",
             to,
             subject,
             react,
         });
-        return {success : true, data};
+        return {success: true, data};
         
     } catch (error) {
         console.log("Failed to send email", error);
-        return {success : false , error};        
+        return {success: false, error};        
     }
 }
